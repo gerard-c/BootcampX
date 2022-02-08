12b1e3bdad133e7edd066f263c8f334222218261
Gerard Compion <gcompion93@gmail.com>
2022-02-08 09:05:24 -0500

    add brackets to math for 'duration'

--- 4_queries/3_assistance_request_data.sql
+++ 4_queries/3_assistance_request_data.sql
@@ -1,4 +1,4 @@
-SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, completed_at - started_at AS duration
+SELECT teachers.name AS teacher, students.name AS student, assignments.name AS assignment, (completed_at - started_at) AS duration
   FROM assistance_requests
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
