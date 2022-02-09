a8b52d67b4c50be41c8e167912eb5dde7bd495a0
Gerard Compion <gcompion93@gmail.com>
2022-02-09 17:24:55 -0500

    add JS to make a query and log its results to console

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -4,5 +4,18 @@ const pool = new Pool({
   user: 'gerard',
   password: '123',
   host: 'localhost',
-  database: 'bootcamp'
+  database: 'bootcampx'
 });
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+LIMIT 5
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
