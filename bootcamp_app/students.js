32fd7f101c6c9e9cb0d2d4d333b9aa6e52c2a758
Gerard Compion <gcompion93@gmail.com>
2022-02-09 17:36:30 -0500

    update query to use CLI arguments for cohorts.name and LIMIT

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const args = process.argv.slice(2);
 
 const pool = new Pool({
   user: 'gerard',
@@ -11,11 +12,11 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-LIMIT 5
+WHERE cohorts.name LIKE '${args[0]}%'
+LIMIT ${args[1] || 5};
 `)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
-})
-.catch(err => console.error('query error', err.stack));
+}).catch(err => console.error('query error', err.stack));
